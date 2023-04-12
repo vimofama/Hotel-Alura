e7d91ee99fd723291e13341a7b7143485e558220
vimofama <vimofama@outlook.com>
2023-04-12 10:48:04 -0500

    Agregando el main del proyecto

---
+++ src/HotelAluraMain.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+import views.Login;
+import views.MenuPrincipal;
+
+public class HotelAluraMain {
+
+	public static void main(String[] args) {
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
+		menuPrincipal.setVisible(true);
+	}
+
+}
